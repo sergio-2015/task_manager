9a6faddf74ab6ddfd39ea45ead88a9417e206a56
sergio monteiro <sergio.perso@gmail.com>
2015-10-27 12:49:34 +0100

    adding routes / actions / views for index, create, show

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,30 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def show
+  end
+
+
+
+private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :done)
+  end
+
+
+
+end
