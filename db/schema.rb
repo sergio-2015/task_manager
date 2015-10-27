021de5025caab724e8fb99bcbd1d94d2ebea90ec
sergio monteiro <sergio.perso@gmail.com>
2015-10-27 11:45:59 +0100

    setting the right entries in the db

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027103041) do
+ActiveRecord::Schema.define(version: 20151027104324) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "description"
     t.boolean  "done"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "name"
   end
 
 end
